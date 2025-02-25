02ef1c60e36bf22d2a42ca5ec8b91349356b31b9
L17 <a.alabdullatif.17@gmail.com>
2025-02-25 06:55:58 -0800

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,46 @@
+// Simple service worker using Workbox or a custom approach
+
+// Cache name and assets to cache
+const CACHE_NAME = 'flower-pwa-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './manifest.json',
+  './images/icon-192.png',
+  './images/icon-512.png'
+];
+
+// Install event
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// Activate event
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch event
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
