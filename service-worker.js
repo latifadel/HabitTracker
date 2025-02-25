1a186d4a74f0652d7af71762c1292a726ecd3812
L17 <a.alabdullatif.17@gmail.com>
2025-02-25 06:30:31 -0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,21 @@
-const cacheName = 'habit-tracker-v1';
-const staticAssets = [
-    './',
-    'index.html',
-    'styles.css',
-    'app.js',
-    'manifest.json'
+const CACHE_NAME = 'flower-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/app.js',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png'
 ];
 
-self.addEventListener('install', async event => {
-    const cache = await caches.open(cacheName);
-    await cache.addAll(staticAssets);
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
 });
 
-self.addEventListener('fetch', event => {
-    event.respondWith(cachFirst(event.request));
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
 });
-
-async function cachFirst(request) {
-    const cachedResponse = await caches.match(request);
-    return cachedResponse || fetch(request);
-}
