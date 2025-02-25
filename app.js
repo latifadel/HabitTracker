0c1a99eb684c5ca42a2629f0895538f12891b8b4
L17 <a.alabdullatif.17@gmail.com>
2025-02-25 06:25:26 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,4 @@
-window.onload = function() {
-    if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.register('/service-worker.js');
-    }
-};
-
-function addHabit() {
-    const name = prompt("Enter the name of the new habit:");
-    if (name) {
-        const template = document.getElementById("habit-template").content.cloneNode(true);
-        template.querySelector(".habit-name").textContent = name;
-        template.querySelector(".delete").onclick = function() {
-            this.parentNode.remove();
-        };
-        document.getElementById("habit-list").appendChild(template);
-    }
-}
+document.getElementById('water-button').addEventListener('click', function() {
+    console.log("Watering the flowers!");
+    // Placeholder for watering logic
+});
